2d78ae3e7db55c11e29b4a1490c4d4c1c5adc172
Terrorskull <terrorskull@terrorskull.com>
2017-07-12 23:43:35 -0500

    Initial conversion to TBC script.

--- StartScript.bat
+++ StartScript.bat
@@ -1,9 +1,9 @@
 ::-----------------------------------------------------------
 :: Name:     StartScript.bat
-:: Purpose:  Configure and start Classic WoW on specified server
+:: Purpose:  Configure and start Burning Crusade WoW on specified server
 :: Author:   terrorskull@terrorskull.com
-:: Version:  1.3
-:: Date:     05JUL2017
+:: Version:  1.0
+:: Date:     12JUL2017
 ::
 ::           Create a copy of this file for each realm.
 ::
@@ -11,14 +11,6 @@
 ::
 ::           Run the executable directly or create a shortcut on the
 ::           desktop (allows for changing the icon).
-:: Modified: 05JUL2017 - 1.3
-::           - Removed merged realms of Kronos II and Zeth'Kur from suggested list
-:: Modified: 20JAN2017 - 1.2
-::			 - Fixed issue with realm names containing a single quote (')
-::			 - Added new Elysium realm of Zeth'Kur to examples
-:: Modified: 16JAN2017 - 1.1
-::			 - Changed Elysium realm names to match removal of Nostalrius data
-::			 - Added realm names for Crestfall
 ::-----------------------------------------------------------
 
 @ECHO OFF
@@ -28,16 +20,16 @@
 ::-----------------------------------------------------------
 
 :: Wow.exe file path - the location storing the classic WoW.exe
-SET DIR=C:\Program Files (x86)\World of Warcraft Classic
+SET DIR=C:\Program Files (x86)\Burning Crusade
 
 :: Account name - generated on site for the server
 SET NAME=myname
 
-:: Realm name - the realm to log into, e.g., Kronos, Anathema, Darrowshire, Elysium, Kul Tiras PvP, Zul'Dare PvE
-SET REALM=Elysium
+:: Realm name - the realm to log into, e.g., Felmyst (Horde), Felmyst (Alliance), Darrowshire, Medivh, Outland
+SET REALM=Felmyst (Horde)
 
 :: Logon server specified in realmlist.wtf file - provided by site hosting the server
-SET LOGON=logon.elysium-project.org
+SET LOGON=game.felmyst.com
 
 ::-----------------------------------------------------------
 :: Automated commands - do not modify
@@ -49,7 +41,7 @@ ECHO Configuring Classic WoW located at %DIR%...
 :: Clear WDB folder
 ECHO.
 ECHO Clearing WDB folder...
-del /F /Q "%DIR%\WDB"
+del /F /Q "%DIR%\Cache\WDB"
 
 :: Check for single quote in realm name
 SET OREALM=%REALM%
